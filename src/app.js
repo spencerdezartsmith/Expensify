c7d91060c3e300bd8c5e6508636dfbc87d133899
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-11-12 09:47:16 -0800

    Set up react-router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import 'normalize.css/normalize.css';
-
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Boiler plate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>Some text from the dashboard</div>
+);
+
+const AddExpensePage = () => (
+  <div>Some text from the add expense page</div>
+);
+
+const EditExpensePage = () => (
+  <div>Some text from the edit expense page</div>
+);
+
+const HelpPage = () => (
+  <div>Some text from the help page</div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path='/' component={ExpenseDashboardPage} exact={true} />
+      <Route path='/create' component={AddExpensePage} />
+      <Route path='/edit' component={EditExpensePage} />
+      <Route path='/help' component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
